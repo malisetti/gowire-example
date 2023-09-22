d8d4973b6924a675724cb7c425dafd632aea4531
SM <abbiya@gmail.com>
2023-09-22 20:31:35 +0530

    add tests for all sayers

--- hello/hello.go
+++ hello/hello.go
@@ -8,30 +8,31 @@ import (
 const Message = "Hello, World!"
 
 type Sayer interface {
-	Say(w io.Writer, msg string)
+	Say(w io.Writer, msg string) (n int, err error)
 }
 
-var DefaultSayer NewLineSayer
+var defaultSayer NewLineSayer
 
 func Say(w io.Writer, msg string) {
-	DefaultSayer.Say(w, msg)
+	defaultSayer.Say(w, msg)
 }
 
 type NewLineSayer struct{}
 
-func (nls NewLineSayer) Say(w io.Writer, msg string) {
-	fmt.Fprintln(w, msg)
+func (nls NewLineSayer) Say(w io.Writer, msg string) (int, error) {
+	return fmt.Fprintln(w, msg)
 }
 
 type CopySayer struct{}
 
-func (cp CopySayer) Say(w io.Writer, msg string) {
-	w.Write([]byte(msg))
+func (cp CopySayer) Say(w io.Writer, msg string) (int, error) {
+	return w.Write([]byte(msg))
 }
 
 type NoSayer struct{}
 
-func (ns NoSayer) Say(w io.Writer, msg string) {
+func (ns NoSayer) Say(w io.Writer, msg string) (n int, err error) {
 	_ = w
 	_ = msg
+	return
 }
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -1,17 +1,55 @@
-package hello
+package hello_test
 
 import (
 	"bytes"
+	"example/hello"
 	"strings"
 	"testing"
 )
 
 func TestSay(t *testing.T) {
 	var buf bytes.Buffer
-	Say(&buf, Message)
+	hello.Say(&buf, hello.Message)
 
 	output := strings.TrimRight(buf.String(), "\n")
-	if output != Message {
-		t.Fatalf("output: '%s' not equals message: '%s'", output, Message)
+	if output != hello.Message {
+		t.Fatalf("output: '%s' not equals message: '%s'", output, hello.Message)
+	}
+
+	buf.Reset()
+
+	var sayer hello.Sayer
+	sayer = hello.NewLineSayer{}
+	_, err := sayer.Say(&buf, hello.Message)
+	if err != nil {
+		t.Fatalf("default sayer could not say %s", hello.Message)
+	}
+
+	output = strings.TrimRight(buf.String(), "\n")
+	if output != hello.Message {
+		t.Fatalf("output: '%s' not equals message: '%s'", output, hello.Message)
+	}
+
+	buf.Reset()
+
+	sayer = hello.CopySayer{}
+	_, err = sayer.Say(&buf, hello.Message)
+	if err != nil {
+		t.Fatalf("copy sayer could not say %s", hello.Message)
+	}
+	output = buf.String()
+	if output != hello.Message {
+		t.Fatalf("output: '%s' not equals message: '%s'", output, hello.Message)
+	}
+
+	buf.Reset()
+
+	sayer = hello.NoSayer{}
+	n, err := sayer.Say(&buf, hello.Message)
+	if err != nil {
+		t.Fatalf("no sayer could not say %s", hello.Message)
+	}
+	if n > 0 || len(buf.String()) > 0 {
+		t.Fatalf("output is not empty")
 	}
 }
