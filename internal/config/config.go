2ecf42921102aa47b0c24c9ba689ab3478ad2ae6
SM <abbiya@gmail.com>
2023-09-23 14:17:16 +0530

    get config from flags

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"example/hello"
+	"flag"
 	"sync"
 
 	"github.com/google/wire"
@@ -24,7 +25,13 @@ var once sync.Once
 
 func GetCfg() *Configuration {
 	once.Do(func() {
-		cfg.TransformerProviderType = hello.NewLineTransform
+		var transformType int
+		flag.IntVar(&transformType, "transform", int(hello.ExactTransform), "ZeroTransform 0, NewLineTransform 1, ExactTransform 2")
+		flag.Parse()
+		if transformType < int(hello.ZeroTransform) || transformType > int(hello.ExactTransform) {
+			panic("invalid transform")
+		}
+		cfg.TransformerProviderType = hello.TransformType(transformType)
 	})
 
 	return &cfg
